cdd750ce4d103a98938b34996153fe0179912608
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-08-15 21:26:46 -0600

    initial prompt & character parameter confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,21 @@
-// Assignment code here
+function generatePassword() {
+    charLength = prompt("How many characters would you like your password to be? Choose between 8 and 128.");
+
+     if (!charLength) {
+        alert("This needs a value");
+        generatePassword();
+
+    } else if (charLength < 8 || charLength > 128) {
+        alert("You must choose between 8 and 128");
+        generatePassword();
+
+    } else {
+        confirmNumber = confirm("Should your password contain numbers?");
+        confirmCharacter = confirm("Should your password contain special characters?");
+        confirmUppercase = confirm("Should your password contain UPPERCASE letters?");
+        confirmLowercase = confirm("Should your password contain lowercase letters?");
+    };
+};
 
 
 // Get references to the #generate element
@@ -8,7 +25,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
